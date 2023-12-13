2cb9fbce15b1cd63838ea9fb0b70b94489c4613b
Michael Kinnas <michaelkinnas@gmail.com>
2023-12-13 20:55:54 +0200

    training func

--- Excersise2/MLProject2Dataset.py
+++ Excersise2/MLProject2Dataset.py
@@ -1,4 +1,8 @@
 import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torch import optim
 import torchvision
 import pandas as pd
 import glob
@@ -6,7 +10,7 @@ import os
 from pathlib import Path
 import PIL
 
-class MLProject2Dataset(torch.utils.data.Dataset):
+class MLProject2Dataset(Dataset):
     def __init__(self, data_dir, metadata_fname='metadata.csv', transform=None):
         self.data_dir = data_dir
         self.transform = transform
@@ -18,12 +22,12 @@ class MLProject2Dataset(torch.utils.data.Dataset):
         })
 
         # Create dataframe for metadata
-        self.metadata = pd.read_csv(data_dir + '/'+metadata_fname)
+        self.metadata = pd.read_csv(data_dir + '/' + metadata_fname)
         self.metadata['dx'] = self.metadata['dx'].astype('category').cat.codes
-        
 
         # Create main dataframe
-        self.dataset = pd.concat([self.files['path'],  self.metadata['dx']], axis=1)
+        self.dataset = pd.merge(self.files, self.metadata[['dx', 'image_id']], on='image_id')
+        
 
     def __len__(self):
         return len(self.dataset)
@@ -31,3 +35,32 @@ class MLProject2Dataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         return (torchvision.io.read_image(self.dataset.at[idx, 'path']).to(torch.float32) / 255, self.dataset.at[idx, 'dx'])
       
+
+def image_transforms(m, n):
+    return transforms.Compose([
+                transforms.Resize((m, n)),
+                transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+            ])
+
+# def train(model: nn.Module, trainloader: DataLoader, valloader: DataLoader = None, 
+#           epochs: int = 10, optimizer: optim = None, loss: nn.modules.loss = None,
+#           device: str = 'cpu', print_period: int = 10) -> None:
+    
+    
+#     size = len(dataloader.dataset)
+#     model.train()
+#     for batch, (X, y) in enumerate(dataloader):
+#         X, y = X.to(device), y.to(device)
+#         pred = model(X)
+#         loss = loss_fn(pred, y)
+
+#         # Compute gradients with backpropagation
+#         loss.backward()
+#         # Execute an optimization step
+#         optimizer.step()
+#         # Need to zero out our gradients, since loss.backward() accumulates losses
+#         optimizer.zero_grad()
+
+#         if batch % 100 == 0:
+#             loss, current = loss.item(), (batch+1)*len(X)
+#             print(f"loss: {loss:>7f} [{current:>5d} / {size:>5d}]")
